129614327674d2e2742412a96cd69ddc0c86a22c
Jess Chadwick <jesschadwick@gmail.com>
2023-12-14 10:10:07 -0500

    Fix meal calender events

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-const AnyList = require("./lib");
+const {env} = require('process');
+const AnyList = require('./lib');
 
 const any = new AnyList({
-  email: process.env.EMAIL,
-  password: process.env.PASSWORD,
+	email: env.EMAIL,
+	password: env.PASSWORD,
 });
 
-any.login().then(async () => {
-  const newEvent = await any.createMealPlanningCalendarEvent({
-    title: "Test",
-    details: "this is a test"
-  });
+await any.login().then(async () => {
+	const recipes = await any.getRecipes();
 
-  await newEvent.save();
+	// Create new event
+	const newEvent = await any.createMealPlanningCalendarEvent({
+		title: 'Test',
+		details: 'this is a test',
+		recipeId: recipes[0].identifier,
+	});
+	await newEvent.save();
 
-  any.teardown();
+	// Update saved event (change recipe ID)
+	const savedEvents = await any.getMealPlanningCalendarEvents();
+	const savedEvent = savedEvents.find(x => x.identifier === newEvent.identifier);
+	if (!savedEvent) {
+		throw new Error('Failed to find saved event');
+	}
+
+	savedEvent.recipeId = recipes[1].identifier;
+	await savedEvent.save();
+
+	const updatedEvents = await any.getMealPlanningCalendarEvents();
+	const updatedEvent = updatedEvents.find(x => x.identifier === newEvent.identifier);
+	if (!updatedEvent) {
+		throw new Error('Failed to find updated event');
+	}
+
+	if (updatedEvent.recipeId !== recipes[1].identifier) {
+		throw new Error('Event was not updated');
+	}
+
+	// Delete test event
+	await savedEvent.delete();
+	const updatedEvents2 = await any.getMealPlanningCalendarEvents();
+	const deletedEvent = updatedEvents2.find(x => x.identifier === newEvent.identifier);
+	if (deletedEvent) {
+		throw new Error('Failed to delete event');
+	}
+
+	any.teardown();
 });
